95d931aa22ec651c01a7c110f85bcff22c431fb3
Nguyen Hieu Thanh <nguyen.hieuthanh@outlook.com>
2025-10-13 23:20:40 +0700

    Update module_test
    Make test output more readable

--- libs/module_test.py
+++ libs/module_test.py
@@ -1,8 +1,48 @@
+import time
 import inspect
 
-# ANSI escape codes for colors
-RED = "\033[91m"
-GREEN = "\033[92m"
+# Output terminal color constants
+# Format
+BOLD = "\033[1m"
+ITALIC = "\u001b[3m"
+
+# Foreground
+FG_BLACK = "\033[30m"
+FG_RED = "\033[31m"
+FG_GREEN = "\033[32m"
+FG_YELLOW = "\033[33m"
+FG_BLUE = "\033[34m"
+FG_MAGENTA = "\033[35m"
+FG_CYAN = "\033[36m"
+FG_WHITE = "\033[37m"
+FG_MAGENTA = "\x1b[35m"
+FG_BRIGHT_BLACK = "\033[90m"  # Also known as Dark Gray
+FG_BRIGHT_RED = "\033[91m"
+FG_BRIGHT_GREEN = "\033[92m"
+FG_BRIGHT_YELLOW = "\033[93m"
+FG_BRIGHT_BLUE = "\033[94m"
+FG_BRIGHT_MAGENTA = "\033[95m"
+FG_BRIGHT_CYAN = "\033[96m"
+FG_BRIGHT_WHITE = "\033[97m"
+
+# Background
+BG_BLACK = "\033[40m"
+BG_RED = "\033[41m"
+BG_GREEN = "\033[42m"
+BG_YELLOW = "\033[43m"
+BG_BLUE = "\033[44m"
+BG_MAGENTA = "\033[45m"
+BG_CYAN = "\033[46m"
+BG_WHITE = "\033[47m"
+BG_BRIGHT_BLACK = "\033[100m"
+BG_BRIGHT_RED = "\033[101m"
+BG_BRIGHT_GREEN = "\033[102m"
+BG_BRIGHT_YELLOW = "\033[103m"
+BG_BRIGHT_BLUE = "\033[104m"
+BG_BRIGHT_MAGENTA = "\033[105m"
+BG_BRIGHT_CYAN = "\033[106m"
+BG_BRIGHT_WHITE = "\033[107m"
+
 RESET = "\033[0m"
 
 def do_test(function_name: str, should_return, **kwargs) -> bool:
@@ -24,10 +64,15 @@ def do_test(function_name: str, should_return, **kwargs) -> bool:
     args = ", ".join([f"{key}={value}" for key, value in kwargs.items()])
     test_function = inspect.currentframe().f_back.f_globals[function_name]
 
+    print(f"{BG_BRIGHT_CYAN+FG_BLACK}TESTING{RESET} {BOLD}{function_name}{RESET}{FG_BRIGHT_BLACK}({args}){RESET}")
+
     # Test
+    start_time = time.time()
     test_result = test_function(**kwargs)
+    end_time = time.time()
 
     # Show test result
-    print(f"{function_name}({args}) -> {test_result}")
-    print("\t->",GREEN + "PASS" if test_result == should_return else RED + "FAIL", RESET)
+    print(f"    -> {test_result} {FG_BRIGHT_BLACK}({round((end_time - start_time) * 1000, 10)} ms){RESET}")
+    print(f"    -> {FG_GREEN + "PASS" if test_result == should_return else FG_RED + "FAIL"}{RESET}")
     return test_result == should_return
+
